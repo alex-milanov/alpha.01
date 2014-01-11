694d4f75eca28316de3db6b0e81d2c900dc8cb5b
Aleksandrenko <aleksandrenko@gmail.com>
2014-01-11 12:53:53 +0200

    geolocation added, try uuid but fail.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,12 +2,31 @@ angular.module('starter.controllers', [])
 
 .controller('WelcomeCtrl', function($scope) {
 
+    $scope.fbLogin = function() {
+        alert("facebook login");
+    }
+
+    $scope.deviceInfo = function() {
+        alert('device info');
+        console.log(device);
+    }
+
+    $scope.getGeoLocation = function() {
+        alert('get geolocation');
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+            alert('post: ' + position.coords.latitude + ', long: ' + position.coords.longitude + ', accuracy: ' + position.coords.accuracy + ', time: ' + position.timestamp);
+        }, function(error) {
+            alert('code: ' + error.code + '\n message: ' + error.message);
+        });
+    }
 })
 
 .controller('PickImageSourceCtrl', function($scope) {
 
-    $scope.useCamera = function() {
 
+
+    $scope.useCamera = function() {
         alert('useCamera');
 
         var cameraOptions = { quality : 75,
